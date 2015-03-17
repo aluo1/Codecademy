e4b268a8eae117f49af5497035b84c5f08af958a
An Luo <martin940425@gmail.com>
2015-03-17 20:15:46 +1100

    upload welcome.rb
    This file contains the function called welcome.rb, which is the answer
    of Codecademy Ruby

---
+++ Ruby/Blocks-and-Sorting/welcome.rb
@@ -0,0 +1,13 @@
+=begin
+This file contains just one function called welcome
+It takes on argument and just print out "Welcome to Ruby!"
+
+Author: Martin Luo
+Date Created: 17 March 2015
+=end
+
+def welcome
+    puts "Welcome to Ruby!"
+end
+
+welcome
